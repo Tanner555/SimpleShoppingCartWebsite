1304ec27dd94bbbf3ae2a86d76872c80ec839bcf
Tanner555 <theman989898@gmail.com>
2019-03-17 14:52:44 -0700

    Added Website Files

---
+++ OtherClasses/MyDBController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.EntityFrameworkCore;
+using MySql.Data.EntityFrameworkCore.Extensions;
+using MySql.Data.MySqlClient;
+using myPrivateInfo = CoreWebsiteTest1.PrivateInfo.MyPrivateInfo;
+
+namespace CoreWebsiteTest1.OtherClasses
+{
+    public class MyDBController : IDisposable
+    {
+        private MySqlConnection PrivateConn;
+        private MySqlCommand MyPrivateSqlCommand;
+        private MySqlDataReader MyPrivateSqlDataReader;
+        
+        private string myConnectionString
+        {
+            //Template:server=servername;uid=userID;password=userPassword;database=databaseName
+            get { return myPrivateInfo.mySQLConnectionString; }
+        }
+
+        public bool connectDB(out MySqlConnection conn, out string connectionErrnoIfAny)
+        {
+            conn = null; connectionErrnoIfAny = "";
+            if (PrivateConn != null && !string.IsNullOrEmpty(PrivateConn.Database))
+            {
+                conn = PrivateConn;
+                return true;
+            }
+
+            bool bconnectionwassuccessful = false;
+            try
+            {
+                bconnectionwassuccessful = true;
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                bconnectionwassuccessful = false;
+                connectionErrnoIfAny = ex.Message;
+            }
+            return bconnectionwassuccessful;
+        }
+
+        public bool RunQuery(string query, out MySqlDataReader dataReader, out string connectionErrnoIfAny)
+        {
+            MySqlConnection conn; dataReader = null;
+            if(connectDB(out conn, out connectionErrnoIfAny))
+            {
+                return RunQuery(query, conn, out dataReader, out connectionErrnoIfAny);
+            }
+            return false;
+        }
+
+        public bool RunQuery(string query, MySqlConnection conn, out MySqlDataReader dataReader, out string connectionErrnoIfAny)
+        {
+            dataReader = null; connectionErrnoIfAny = "";
+            try
+            {
+                if (MyPrivateSqlCommand != null) MyPrivateSqlCommand.Dispose();
+                if (MyPrivateSqlDataReader != null) MyPrivateSqlDataReader.Dispose();
+
+                MyPrivateSqlCommand = new MySqlCommand(query, conn);
+                MyPrivateSqlDataReader = MyPrivateSqlCommand.ExecuteReader();
+                dataReader = MyPrivateSqlDataReader;
+                return true;
+            }
+            catch(Exception ex)
+            {
+                connectionErrnoIfAny = ex.Message;
+                return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (PrivateConn != null)
+            {
+                PrivateConn.Dispose();
+                PrivateConn = null;
+            }
+            if (MyPrivateSqlCommand != null)
+            {
+                MyPrivateSqlCommand.Dispose();
+                MyPrivateSqlCommand = null;
+            }
+            if (MyPrivateSqlDataReader != null)
+            {
+                MyPrivateSqlDataReader.Dispose();
+                MyPrivateSqlDataReader = null;
+            }
+
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.EntityFrameworkCore;
+using MySql.Data.EntityFrameworkCore.Extensions;
+using MySql.Data.MySqlClient;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CoreWebsiteTest1.OtherClasses;
+using dbHandle = CoreWebsiteTest1.OtherClasses.MyDBController;
+
+namespace CoreWebsiteTest1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //public MySql.Data.MySqlClient.MySqlConnection conn;
+        //public string myConnectionString;
+        //public bool bConnectionWasSuccessful = false;
+        //public string connectionErrnoIfAny = "";
+
+        public void OnGet()
+        {
+
+        }
+
+        public bool IsPost()
+        {
+            return HttpContext.Request.Method == "POST";
+        }
+
+        //public bool connectDB(out MySqlConnection conn, out string connectionErrnoIfAny)
+        //{
+        //    return dbHandle.connectDB(out conn, out connectionErrnoIfAny);
+        //}
+    }
+}
