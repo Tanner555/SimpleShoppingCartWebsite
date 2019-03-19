befb3ed472ca52611073406f17340344d037554a
Tanner555 <theman989898@gmail.com>
2019-03-19 01:38:15 -0700

    Fixed Quantity Parse Error When Using Wrong Type In Field

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -41,7 +41,10 @@ namespace CoreWebsiteTest1.Pages
                     case "add":
                         if (Request.Form.ContainsKey("quantity") && !string.IsNullOrEmpty(_code))
                         {
-                            int _quantity = int.Parse(Request.Form["quantity"]);
+                            int _quantity;
+                            bool _bQuantityParsed = int.TryParse(Request.Form["quantity"], out _quantity);
+                            if (_bQuantityParsed == false) _quantity = 1;
+
                             string _productByCodeQuery = "SELECT * FROM products WHERE code='" + _code + "'";
                             List<ProductItemModel> _productItems; string _connectionErrnoIfAny;
                             if (MyModelsHandler.RetrieveProductItemsFromQuery(_productByCodeQuery, out _productItems, out _connectionErrnoIfAny) &&
